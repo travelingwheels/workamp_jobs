6f8653c1418148ef1a9f87320bcfa4abb85147d0
Gary Wheeler <gkwheeler40@gmail.com>
2020-01-25 18:33:18 +0000

    stub out first method

--- lib/workamp_jobs/cli.rb
+++ lib/workamp_jobs/cli.rb
@@ -2,6 +2,12 @@ class WorkampJobs::Cli
 
   def call
     puts "Welcome to Workamp jobs!"
+    job_list
   end
   
+  def job_list
+    puts "Here is today's list of workamp jobs."
+    puts "Equestrian Trail guide - Mount Carmel, UT - seasonal."
+    puts "Cooks, Servers, and Hosts - Mount Carmel, UT - seasonal."
+  end
 end
