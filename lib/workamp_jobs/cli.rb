6ed3ac3630ffaca06d1f2967b246376d31d3bafb
Gary Wheeler <gkwheeler40@gmail.com>
2020-01-27 01:04:11 +0000

    minor adjustment to methods

--- lib/workamp_jobs/cli.rb
+++ lib/workamp_jobs/cli.rb
@@ -1,13 +1,13 @@
 class WorkampJobs::Cli
 
   def call
-    puts "Welcome to Workamp jobs!"
     job_list
     menu
     goodbye
   end
   
   def job_list
+    puts "Welcome to Workamp jobs!"
     puts "Here is today's list of workamp jobs."
     puts "Equestrian Trail guide - Mount Carmel, UT - seasonal."
     puts "Cooks, Servers, and Hosts - Mount Carmel, UT - seasonal."
