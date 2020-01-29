856265f3deecf72110f7506d81367ba2ebba16df
Gary Wheeler <gkwheeler40@gmail.com>
2020-01-29 13:10:45 +0000

    add more requirements to enviroment file

--- lib/workamp_jobs.rb
+++ lib/workamp_jobs.rb
@@ -1,7 +1,10 @@
 require_relative "./workamp_jobs/version"
 require_relative "./workamp_jobs/cli"
+require_relative "./workamp_jobs/jobs"
+require_relative "./workamp_jobs/scraper"
 
 require 'pry'
+require 'nokogiri'
 
 #module WorkampJobs
  # class Error < StandardError; end
--- lib/workamp_jobs/scraper.rb
+++ lib/workamp_jobs/scraper.rb
@@ -1,2 +1,4 @@
 class WorkampJobs::Scraper
+  
+  
 end
