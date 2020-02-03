1ef5e787e607e422c5185ef7f18cfc83971e397a
Gary Wheeler <gkwheeler40@gmail.com>
2020-02-03 10:22:34 +0000

    adjust attributes in jobs file

--- lib/workamp_jobs/jobs.rb
+++ lib/workamp_jobs/jobs.rb
@@ -1,10 +1,12 @@
 class WorkampJobs::Jobs
   
-  attr_accessor :title, :location, :post_date, :ref
+  attr_accessor :title, :location, :post_date
   @@all = []
   
   def initialize(title)
     @title = title
+    @location = location
+    @post_date = post_date
     save
   end
   
--- lib/workamp_jobs/scraper.rb
+++ lib/workamp_jobs/scraper.rb
@@ -2,5 +2,8 @@ class WorkampJobs::Scraper
   
   def self.scrape_jobs
     doc = Nokogiri::HTML(open("https://www.coolworks.com/jobs-with-rv-spaces"))
+    jobs = doc.css("")
+    WorkampJobs::jobs.new
+    binding.pry
   end
 end
