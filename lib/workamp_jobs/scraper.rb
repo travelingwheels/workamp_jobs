ea6ce8b119ff5108fc0beba3a2fd9afe94584813
Gary Wheeler <gkwheeler40@gmail.com>
2020-02-05 22:30:12 +0000

    sort out scraper method

--- lib/workamp_jobs/scraper.rb
+++ lib/workamp_jobs/scraper.rb
@@ -2,21 +2,20 @@ class WorkampJobs::Scraper
   
   def self.scrape_jobs
     doc = Nokogiri::HTML(open("https://www.coolworks.com/jobs-with-rv-spaces"))
-    #WorkampJobs::Jobs.new#(title, location, post_date)
-    jobs = self.new
-    jobs.title = title.css("div.text").text
-    #jobs.location = doc.css("div.location").text
-    #jobs.post_date = doc.css("div.bt-box").text.strip
+    
+    jobs = doc.css("div.container")
+    
+    jobs.each do |job|
+    
+    title = doc.css("div.text").css("div.top-meta h4 a").map(&:text)
+    location = doc.css("div.location").map(&:text)
+    post_date = doc.css("section.job-post-row").css("div.wrap").map(&:text)
+    
+     WorkampJobs::Jobs.new(title, location, post_date)
+    end
   end
 end
- ##option 3 - The Clean Model Method
-      ##using the writer/setter method from the scraper
 
-        # game_object = Game.new
-        # home_and_team = game.css("div.game-info").text.split
-        # game_object.team = home_and_team[1]
-        # game_object.home = home_and_team[0]
-        # game_object.date =  game.css("div.game-date").text
-        # game_object.score = game.css("div.score").text
-        # game_object.result = game.css("div.game-result").text
-   # end
+#WorkampJobs::Jobs.new#(title, location, post_date)
+#jobs = self.new
+    #binding.pry
