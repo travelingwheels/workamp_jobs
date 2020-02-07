2a8b79b32d734b1dd9feb088208a86358dd52f9e
Gary Wheeler <gkwheeler40@gmail.com>
2020-02-07 12:21:02 +0000

    scraper class has good data

--- lib/workamp_jobs/scraper.rb
+++ lib/workamp_jobs/scraper.rb
@@ -3,21 +3,15 @@ class WorkampJobs::Scraper
   def self.scrape_jobs
     doc = Nokogiri::HTML(open("https://www.coolworks.com/jobs-with-rv-spaces"))
     
-    section = doc.css("div.content-holder")
-    
-    jobs = section.css("section.job-post-row")
+    jobs = doc.css("div.content-holder")
     
     jobs.each do |job|
     
-    title = job.css("div.top-meta h4 a").text
-    location = job.css("div.location").text
-    post_date = job.css("section.job-post-row").css("div.wrap").text
-    binding.pry
+    title = job.css("div.top-meta h4 a").map(&:text)
+    location = job.css("div.location").map(&:text)
+    post_date = job.css("span.time").map(&:text)
+    #binding.pry
      WorkampJobs::Jobs.new(title, location, post_date)
     end
   end
 end
-
-#WorkampJobs::Jobs.new#(title, location, post_date)
-#jobs = self.new
-    #binding.pry
